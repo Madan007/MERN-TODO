3421c26f83d67ea98fa21fce9fddb637d95d00f8
Retina <retina@PT-LTP-011-MAC.local>
2020-05-13 14:28:59 +0530

    Updated

--- BackEnd/common/constants.js
+++ BackEnd/common/constants.js
@@ -1,7 +1,8 @@
 const APP_CONSTANTS = {
   API_URL: '/api/v1/to-do',
   PORT: 8000,
-  MONGO_URL: '',
+  MONGO_URL:
+    'mongodb+srv://madan:madan007%24@cluster0-xezi2.mongodb.net/todos?retryWrites=true&w=majority',
 };
 
 module.exports = {
--- client/src/App.js
+++ client/src/App.js
@@ -5,54 +5,57 @@ import TodosList from './components/todos-list.component';
 import CreateTodo from './components/create-todo.component';
 import EditTodo from './components/edit-todo.component';
 import DeleteTodo from './components/delete-todo.component';
+import ErrorBoundary from './ErrorBoundary';
 
 import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">
-              <img src={logo} width="30" height="30" alt="logo" />
-            </a>
-            <Link to="/" className="navbar-brand">
-              MERN-Stack Todo App
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#myNavbar"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="myNavbar">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">
-                    Todos
-                  </Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">
-                    Create Todo
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+      <ErrorBoundary>
+        <Router>
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a className="navbar-brand" href="/">
+                <img src={logo} width="30" height="30" alt="logo" />
+              </a>
+              <Link to="/" className="navbar-brand">
+                MERN-Stack Todo App
+              </Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#myNavbar"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="myNavbar">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item">
+                    <Link to="/" className="nav-link">
+                      Todos
+                    </Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link to="/create" className="nav-link">
+                      Create Todo
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
 
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
-          <Route path="/delete/:id" component={DeleteTodo} />
-        </div>
-      </Router>
+            <Route path="/" exact component={TodosList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
+            <Route path="/delete/:id" component={DeleteTodo} />
+          </div>
+        </Router>
+      </ErrorBoundary>
     );
   }
 }
