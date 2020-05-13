2a879c5c3171d0c913a0fe51c512b57cb31f2b75
Retina <retina@PT-LTP-011-MAC.local>
2020-05-13 14:18:30 +0530

    removed monog ul

--- BackEnd/common/constants.js
+++ BackEnd/common/constants.js
@@ -1,8 +1,7 @@
 const APP_CONSTANTS = {
   API_URL: '/api/v1/to-do',
   PORT: 8000,
-  MONGO_URL:
-    'mongodb+srv://madan:madan007%24@cluster0-xezi2.mongodb.net/todos?retryWrites=true&w=majority',
+  MONGO_URL: '',
 };
 
 module.exports = {
